2e60c977ae2e7ac4c333e93cecc024dc4c68bac8
moritz scheuerle <moritz.scheuerle@siriusonline.de>
2022-03-25 00:41:18 +0100

    added IterableStream for iterating tokens in the lexer

---
+++ IterableStream.h
@@ -0,0 +1,38 @@
+//
+// Created by Mo on 24.03.2022.
+//
+
+#ifndef DATA_PROCESSING_INTERMEDIATE_ITERABLESTREAM_H
+#define DATA_PROCESSING_INTERMEDIATE_ITERABLESTREAM_H
+
+
+#include <utility>
+
+#include "Tokenizer.h"
+
+class IterableStream {
+private:
+    std::vector<Token> *v;
+    uint64_t index = 0;
+public:
+    IterableStream(std::vector<Token> *vector) : v(vector) {
+
+    }
+
+    Token current() {
+        return v->at(index);
+    }
+    bool hasEntriesLeft() {
+        return index < v->size();
+    }
+
+    void consume() {
+        index += 1;
+    }
+    void reset() {
+        index = 0;
+    }
+};
+
+
+#endif //DATA_PROCESSING_INTERMEDIATE_ITERABLESTREAM_H
