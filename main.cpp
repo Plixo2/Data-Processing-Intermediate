e689823e981b5ec85d390d96803076fcc247327a
moritz scheuerle <moritz.scheuerle@siriusonline.de>
2022-03-26 14:54:06 +0100

    filled in the regex for all syntax tokens

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Tokenizer.h"
 #include "IterableStream.h"
+#include "DPI_Syntax.h"
 
 const uint8_t REGISTER = 1;
 const uint8_t STACK = 2;
@@ -39,12 +40,10 @@ typedef union {
 } Instruction;
 
 
-
 #define Register(index) record_stack[activation_record_pointer+(index)]
 
 uint64_t activation_record_pointer = 0;
 Object *record_stack;
-
 
 
 const uint64_t stack_size_default = 64;
@@ -55,26 +54,61 @@ int main() {
     std::cout << sizeof(Op_Code) << std::endl;
     std::cout << sizeof(Instruction) << std::endl;
     std::vector<Token_Capture> caps;
-    caps.push_back({0,"^[a-zA-Z]+$","^[a-zA-Z0-9_\\-]+$"});
-    caps.push_back({1,"^\\s$","^\\s+$"});
+
+    caps.push_back(Syntax::BRACES_OPEN);
+    caps.push_back(Syntax::BRACES_CLOSED);
+    caps.push_back(Syntax::PARENTHESES_OPEN);
+    caps.push_back(Syntax::PARENTHESES_CLOSED);
+    caps.push_back(Syntax::A_PLUS);
+    caps.push_back(Syntax::A_MINUS);
+    caps.push_back(Syntax::A_MULTIPLY);
+    caps.push_back(Syntax::A_DIVIDE);
+    caps.push_back(Syntax::A_MOD);
+    caps.push_back(Syntax::A_POW);
+    caps.push_back(Syntax::SMALLER);
+    caps.push_back(Syntax::SMALLER_EQUAL);
+    caps.push_back(Syntax::GREATER);
+    caps.push_back(Syntax::GREATER_EQUAL);
+    caps.push_back(Syntax::EQUALS);
+    caps.push_back(Syntax::TRUE);
+    caps.push_back(Syntax::FALSE);
+    caps.push_back(Syntax::APPROX_EQUALS);
+    caps.push_back(Syntax::NOT_EQUALS);
+    caps.push_back(Syntax::B_AND);
+    caps.push_back(Syntax::B_OR);
+    caps.push_back(Syntax::B_NOT);
+    caps.push_back(Syntax::ASSIGN);
+    caps.push_back(Syntax::END_OF_STATEMENT);
+    caps.push_back(Syntax::KEYWORD);
+    caps.push_back(Syntax::NUMBER);
+    caps.push_back(Syntax::WHITESPACE);
 
     Tokenizer tokenizer(caps);
-    std::string str = "HELLO world";
+    std::string str = "HELLO *true / tru+ tr ^ 2 -t ~= true < > *= != && >= <= (  { ) } !false !=hello && truea; || atrue === true";
     const char *string = str.c_str();
-    std::vector<Token>* stream = tokenizer.generate_stream(string, str.length());
+    std::vector<Token> *stream = tokenizer.generate_stream(string, str.length());
+    stream->push_back({Syntax::_EOF.type, "", str.length(), str.length()});
 
-    IterableStream iterableStream(stream);
-
-    while(iterableStream.hasEntriesLeft()) {
-        const Token &current = iterableStream.current();
-        std::cout << current.raw_string << " | " << current.type << std::endl;
-        iterableStream.consume();
-    }
 
     std::vector<Token> filtered(*stream);
-    filtered.erase(std::remove_if(filtered.begin(), filtered.end(),  [](const Token& token) {return token.type == 1;}) , filtered.end());
-    std::cout<< filtered.size() << std::endl;
-    std::cout<< stream->size() << std::endl;
+    filtered.erase(std::remove_if(filtered.begin(), filtered.end(), [](const Token &token) {
+        return token.type == Syntax::WHITESPACE.type ||
+               token.type == Syntax::EOL.type ||
+               token.type == Syntax::_EOF.type;
+    }), filtered.end());
+
+//    std::cout << filtered.size() << std::endl;
+//    std::cout << stream->size() << std::endl;
+
+    IterableStream iterableStream(&filtered);
+    uint64_t i = 3;
+    while (iterableStream.hasEntriesLeft()) {
+        const Token &current = iterableStream.current();
+        std::cout << current.type << " | " << current.raw_string << std::endl;
+        i += current.type * i;
+        iterableStream.consume();
+    }
+    std::cout << "h: " << i % 10000 << std::endl;
 
     delete stream;
 
